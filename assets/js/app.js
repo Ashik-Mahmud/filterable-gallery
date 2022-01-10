a85a0b86243bbe03e0d84e5c3d19530878c919e7
ashikmahmud <ashikmamud187@gmail.com>
2022-01-11 02:07:07 +0600

    Created app.js file in developmentApp Branch And do development

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// app.js
+
+// selector 
+const filterBtns = document.querySelectorAll(".filter-btn-groups li"),
+    box = document.querySelectorAll(".single-content");
+
+
+// work with filter btn 
+filterBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+        alert("Site is Under construction! Thanks");
+    })
+})
